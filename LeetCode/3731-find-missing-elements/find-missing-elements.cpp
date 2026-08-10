9c3a892c1e80688d4a324fa096c8a88d7ca03d10
Yashvardhan Borude <yashvardhanborude7@gmail.com>
2026-08-10 22:31:24 +0530

    Time Complexity / Solved [3731] Find Missing Elements (Easy) - LeetCode

--- LeetCode/3731-find-missing-elements/find-missing-elements.cpp
+++ LeetCode/3731-find-missing-elements/find-missing-elements.cpp
@@ -1,1 +1,18 @@
-c
+class Solution {
+public:
+    vector<int> findMissingElements(vector<int>& nums) {
+        int n = nums.size();
+        int mx = INT_MIN;
+        int mn = INT_MAX;
+        for(int i = 0;i<n;i++){
+            if(nums[i] > mx) mx = nums[i];
+            if(nums[i] < mn) mn = nums[i];
+        }
+        set<int>st;
+        vector<int>res;
+        for(int i : nums) st.insert(i);
+        for(int i = mn+1;i < mx;i++){
+            if(st.find(i) == st.end()) res.push_back(i);
+        }
+        return res;
+    }
