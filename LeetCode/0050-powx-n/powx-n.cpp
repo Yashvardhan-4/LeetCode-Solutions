db9f3323ed607646441702408cd685a80a0d211a
Yashvardhan Borude <yashvardhanborude7@gmail.com>
2026-08-12 23:11:25 +0530

    Time Complexity / Solved [0050] Pow(x, n) (Medium) - LeetCode

---
+++ LeetCode/0050-powx-n/powx-n.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(x == 0) return 0;
+    }
+        if(n == 1) return x;
+        if(n == 0) return 1;
+        return x * myPow(x,n-1);
+};
